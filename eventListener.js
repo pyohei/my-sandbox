f130cf76abda3791cdff3ff52352864270692b9c
pyohei <mukaishohei76@gmail.com>
2014-06-03 23:28:56 +0900

    add abount window.evet

--- eventListener.js
+++ eventListener.js
@@ -1,4 +1,15 @@
 var testButton = function () {
+    // "window.event" calls active event
+    // but mozilla doesn't use this.
+    // alternatively, "arguments[0] can use
+    var evt = window.event || arguments[0];
+    // alert(evt);
+    // srcElement is mainly used in IE
+    // (but chrome and safari move)
+    var src = evt.srcElement || evt.target;
+    var name = src.id;
+    alert(name)
+        
     alert("say!!");
 };
 
@@ -19,8 +30,25 @@ var ee =  function () {
             window.addEventListener("load", this.eventTest, false);
         },
         eventTest: function () {
+            /*
+            // "window.event" calls active event
+            // but mozilla doesn't use this.
+            // alternatively, "arguments[0] can use
+            var evt = window.event || arguments[0];
+            // alert(evt);
+            // srcElement is mainly used in IE
+            // (but chrome and safari move)
+            var src = evt.srcElement;
+            alert(src.id);
+            alert(evt.target.id);
+            var name = src.id;
+            alert(name)
+            */
+
             var b = document.getElementById("eventButton");
+            // var b = document.getElementById("eventButton");
             b.addEventListener("click", testButton, false);
+            // b.addEventListener("click", testButton, false);
         }
     };
 };
