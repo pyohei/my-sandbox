e5dc34c79f62f9d2240c3995658309769dec8428
pyohei <mukaishohei76@gmail.com>
2014-08-27 22:28:08 +0900

    init commit

---
+++ compare_def_lambda.py
@@ -0,0 +1,32 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+
+""" Execution time comparision with "function" and "lambda".
+
+"""
+
+def func_(x):
+    if x % 2 == 0:
+        return x ** 1000
+    return x ** 500
+
+lam_ = lambda x: x ** 1000 if x % 2 == 0 else x ** 500
+
+if __name__ == "__main__":
+    from timeit import timeit
+    print "----- function -----"
+    print timeit("[func_(x) for x in xrange(1,1000)]",
+        setup="from __main__ import func_",
+        number=1000)
+    print "----- lambda -----"
+    print timeit("[lam_(x) for x in xrange(1,1000)]",
+        setup="from __main__ import lam_",
+        number=1000)
+    print "----- lambda -----"
+    print timeit("[lam_(x) for x in xrange(1,1000)]",
+        setup="from __main__ import lam_",
+        number=1000)
+    print "----- function -----"
+    print timeit("[func_(x) for x in xrange(1,1000)]",
+        setup="from __main__ import func_",
+        number=1000)
