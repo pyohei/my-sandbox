106f1b3b2b29235bbf67bf0648e6991f8fbd2f00
pyohei <mukaishohei76@gmail.com>
2015-06-21 15:43:06 +0900

    fix to move in apache

--- src/wsgi/base.py
+++ src/wsgi/base.py
@@ -13,6 +13,20 @@ def through_main_path():
     sys.path.append(main_path)
 through_main_path()
 
+# simple_server.pyと同じロジックなの同じようにしたい
+try:
+    import bottle
+except:
+    print "This Server don't install bottle.py..."
+    # If you don't have bottle.py, import from local.
+    def through_bottle_path():
+        dirpath = os.path.dirname(os.path.abspath(__file__))
+        paths = dirpath.split("/")
+        bottle_paths = paths[0:-1] + ["framework", "bottle"]
+        bottle_path = "/".join(bottle_paths)
+        sys.path.append(bottle_path)
+    through_bottle_path()
+    import bottle
 # CREATE bottle import setting
 #CWD = os.getcwd()
 #if not os.path.exists(CWD+"/wsgi/bottle/__init__.py"):
