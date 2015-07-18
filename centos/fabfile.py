a99948c4a402c7af0ad69ba31ffd1efa397f049c
pyohei <mukaishohei76@gmail.com>
2015-07-18 13:17:45 +0900

    add deployment settings

--- centos/fabfile.py
+++ centos/fabfile.py
@@ -2,14 +2,54 @@
 
 """ KFC system deploy 
 
+Deproyment script.
 """
 
 from fabric.api import run
+from fabric.api import cd
 from fabric.api import env
+from fabric.api import sudo
+from fabric.contrib import files
 
-env.hosts = ['192.168.99.1:49222']
+env.hosts = ['192.168.99.1:49122']
 env.user = 'vagrant'
 env.password = 'vagrant'
 
 def test():
-    print run('ls /')
+    print '--- TEST RUN ---'
+    try:
+        run('cd /tmp')
+        run('touch test.txt')
+        run('ls -al')
+        run('pwd')
+        run('git --help')
+    except:
+        pass
+    print '--- TEST EXIT ---'
+
+def deploy():
+    print '--- START DEPLOY ---'
+    if not files.exists('deploy'):
+        __mkdir('deploy')
+    with cd('deploy'):
+        if not files.exists('figureskate-judging'):
+            __clone(
+                'https://github.com/pyohei/figureskate-judging.git', submodule=True)
+        with cd('figureskate-judging/src'):
+            run('git pull')
+            sudo('rsync --delete -a -v -r ./ /var/www/web/')
+    print '--- FINISH DEPLOY ---'
+
+def __mkdir(path, force=False):
+    command = 'mkdir '
+    if force:
+        command += '-p '
+    command += path
+    run(command)
+
+def __clone(path, submodule=False):
+    command = 'git clone '
+    if submodule:
+        command += '--recursive '
+    command += path
+    run(command)
