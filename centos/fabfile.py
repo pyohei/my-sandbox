3a0c7d5c3665c67a4ecb6ae31fc18a16cdafac97
pyohei <mukaishohei76@gmail.com>
2015-07-30 22:46:25 +0900

    add ansible command

--- centos/fabfile.py
+++ centos/fabfile.py
@@ -14,6 +14,7 @@ from fabric.api import cd
 from fabric.api import env
 from fabric.api import sudo
 from fabric.api import local
+from fabric.api import lcd
 from fabric.contrib import files
 
 env.hosts = ['192.168.99.1:49122']
@@ -55,6 +56,10 @@ VM_NAME = 'devserver'
 
 
 def setup_devenv():
+    uninstall = __chk_uninstall()
+    if uninstall:
+        raise OSError('You must install below command¥n %s' % (
+            '¥n'.join(uninstall)))
     basepath = path.dirname(path.abspath(__file__))
     vmpath = path.join(basepath, VM_NAME)
     __make_base(vmpath)
@@ -66,21 +71,23 @@ def setup_devenv():
         shutil.rmtree(vmpath)
         raise OSError(
             'Having Error in your directory')
-
-    u""" 動作確認OK """
-
     try:
         up_servers = []
         for s in SERVERS:
-            __start_vagrant(vmpath, s)
             up_servers.append(s)
+            __start_vagrant(vmpath, s)
+        with lcd(basepath):
+            __provision()
     except Exception as e:
         # vagrant destroy
         print 'ERROR: %s' % e
         for s in up_servers:
-            __rollback(s)
-        u""" コメントアウトするときはくれぐれも注意!! """
-#        shutil.rmtree(vmpath)
+            __destroy_vagrant(vmpath, s)
+        shutil.rmtree(vmpath)
+
+
+def __chk_uninstall():
+    return []
 
 
 def __make_base(vmpath):
@@ -111,8 +118,24 @@ def __start_vagrant(devpath, dirname):
     local('vagrant up')
 
 
-def __rollback(dirname):
-    print 'rollback %s' % dirname
+def __provision():
+    local('ansible-playbook -i hosts playbook.yml')
+
+
+def simple_rollback():
+    basepath = path.dirname(path.abspath(__file__))
+    vmpath = path.join(basepath, VM_NAME)
+    for s in SERVERS:
+        __destroy_vagrant(vmpath, s)
+    shutil.rmtree(vmpath)
+
+
+def __destroy_vagrant(vmpath, dirname):
+    """destory vagrant"""
+    u"""[y/N]をできれば自動で挿入したい"""
+    serverpath = path.join(vmpath, dirname)
+    os.chdir(serverpath)
+    local('vagrant destroy')
 
 
 def __mkdir(path, force=False):
