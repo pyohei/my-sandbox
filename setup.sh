50c44297119b2673ec36f9ccfb2dd5de327e2cf6
pyohei <pyohei@users.noreply.github.com>
2016-08-28 10:15:35 +0900

    Update setup.sh
    Add git install.

--- setup.sh
+++ setup.sh
@@ -19,3 +19,6 @@ sudo apt-get -y install software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get -y update
 sudo apt-get -y install ansible
+
+# I wonder if I install git in this source...
+sudo apt-get -y install git
