d25fe47eccddb91c0122413b7703ab7d88d2896a
pyohei <mukaishohei76@gmail.com>
2016-09-05 23:16:16 +0900

    Add tar rotate

---
+++ shell_tech/tarrotate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#================================================================================
+# tar rotate
+#================================================================================
+
+BACKUP_NUM=10
+
+# CREATE BACKUP
+## CREATE BACKUP_DIR
+## COPY or MOVE BACKUP
+## BACKUP
+
+# DELETE BACKUP
+## COUNT BACKUP
+## DELETE BACKUP
