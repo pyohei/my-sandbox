3f4ccbb8b07042f12610b5275ec99621644b98bb
pyohei <mukaishohei76@gmail.com>
2017-01-13 22:50:03 +0900

    Improve efficiency of script execution.

--- cron-prodictor/parser.py
+++ cron-prodictor/parser.py
@@ -27,22 +27,20 @@ def __create_file_object_by_server():
     server_list = {}
     for server_file in os.listdir(CRON_DIRECTORY_NAME):
         file_path = os.path.join(CRON_DIRECTORY_NAME, server_file)
-        f = open(file_path, 'r')
-        lines = f.readlines()
-        name, __ext = os.path.splitext(server_file)
-        server_list[name] = lines
-        f.close()
+        with open(file_path, 'r') as f:
+            lines = f.readlines()
+            name, __ext = os.path.splitext(server_file)
+            server_list[name] = lines
     return server_list
 
 
 @stop_future_warning
 def main():
-    script_to_server = {}
     targets = {}
 
     for sv, lines in __create_file_object_by_server().items():
         for li in lines:
-            li = li.replace('\n', '')
+            li = li.rstrip()
             if not is_cron_script(li):
                 continue
 
@@ -50,10 +48,6 @@ def main():
             cron_time = nz_crnon[0]
             cron_script = nz_crnon[1]
 
-            if cron_script in script_to_server:
-                script_to_server[cron_script].append(sv)
-            else:
-                script_to_server[cron_script] = [sv]
             # TODO: has to handle error occuring missed cron script.
             c = CronTab(cron_time)
 
@@ -64,15 +58,16 @@ def main():
                     timedelta(seconds=interval_second)
                 if current_time > CRON_TO:
                     break
+                cron_combo = (sv, cron_script)
                 if current_time not in targets:
-                    targets[current_time] = [cron_script]
+                    targets[current_time] = [cron_combo]
                 else:
-                    targets[current_time].append(cron_script)
-    export(targets, script_to_server)
+                    targets[current_time].append(cron_combo)
+    export(targets)
 
 def is_cron_script(cron_script):
-    for c in IGNORE_CASE:
-        if c in cron_script or cron_script == '':
+    for ic in IGNORE_CASE:
+        if ic in cron_script or cron_script == '':
             return False
     return True
 
@@ -81,16 +76,16 @@ def normalize_cron_script(cron_script):
     result_list = result.split('\t')
     return (' '.join(result_list[0:5]), result_list[-1])
 
-def export(targets, servers):
+def export(targets):
+    # TODO: separate sort from expot.
     with open('result.csv', 'w') as f:
         header = 'date, hour, miniute, second, scrip, server\n'
         f.write(header)
         for t, scripts in sorted(targets.items()):
             for s in scripts:
-                server_name = ' or '.join(list(set(servers[s])))
                 result = '{0},{1},{2}\n'.format(t.strftime('%Y-%m-%d,%H,%M,%S'),
-                                                s,
-                                                server_name)
+                                                s[1],
+                                                s[0])
                 f.write(result)
 
 if __name__ == '__main__':
