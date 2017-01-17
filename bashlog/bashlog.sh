d9bb3b76c7073d0874ac46aec1955af1cfa855c0
pyohei <mukaishohei76@gmail.com>
2017-01-17 23:01:43 +0900

    Create logging functions.

--- bashlog/bashlog.sh
+++ bashlog/bashlog.sh
@@ -2,11 +2,33 @@
 
 # Logging tool.
 
+# Load config file.
 . config.sh
 
-logging() {
-    LOGGING_DATETIME=`date "+%F %T"`
-    echo "$LOGGING_DATETIME $1" >> $2
+
+LOG_FILE_PATH=
+
+# Init logging
+#   $1 ... Log dir name
+createLogging() {
+    if [ ! -e $1 ]; then
+        mkdir -p $1
+    fi
+    local log_name=`date +"%Y%m%d"`
+    LOG_FILE_PATH=$1/${log_name}.log
+    if [ ! -e ${LOG_FILE_PATH} ]; then
+        touch ${LOG_FILE_PATH}
+    fi
 }
 
-logging test /tmp/hoge.txt
+
+# Logging process.
+#   $1 ... Message for logging.
+logging() {
+    if [ ! -e ${LOG_FILE_PATH} ]; then
+        echo "Log file is not found. You should call 'createLogging')"
+        exit 1
+    fi
+    LOGGING_DATETIME=`date "+%F %T"`
+    echo "$LOGGING_DATETIME $1" >> ${LOG_FILE_PATH}
+}
