acc173d391358f52b8f387511748e9e33fff71e7
pyohei <mukaishohei76@gmail.com>
2017-01-21 10:59:37 +0900

    Add setup script detail.

--- prodictor/setup.py
+++ prodictor/setup.py
@@ -1,2 +1,8 @@
 from setuptools import setup
-setup(name='prodictor', version='0.0.1')
+setup(name='prodictor',
+      version='0.0.1',
+      description='Cron scheduler.',
+      author='Shohei Mukai',
+      author_email='xxxx@mail.mail',
+      url='http://www.www'
+      )
