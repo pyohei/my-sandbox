923047639e865e7c81474a2cbae00d2d2c36745e
pyohei <mukaishohei76@gmail.com>
2017-01-25 22:30:07 +0900

    Add details of setup data.

--- prodictor/setup.py
+++ prodictor/setup.py
@@ -1,9 +1,24 @@
-from setuptools import setup
+from setuptools import setup#, find_packages
+
+long_desc = ''
+with open('README.md') as f:
+    logn_desc = f.read()
+
 setup(name='prodictor',
       version='0.0.1',
       description='Cron scheduler.',
+      logn_description=long_desc,
+      classifiers=[
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.7',
+          'License :: OSI Approved :: MIT Licenseo',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+          ],
+      keywords='cron crontab schedule',
       author='Shohei Mukai',
       author_email='xxxx@mail.mail',
       url='http://www.www',
+      license='MIT',
+      # packages=find_packages,
       install_requires=['crontab']
       )
