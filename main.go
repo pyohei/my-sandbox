b563064bff7c6054f11df9f2c378fa87f40eb684
pyohei <mukaishohei76@gmail.com>
2017-11-23 22:25:18 +0900

    Add new go file.

--- main.go
+++ main.go
@@ -2,3 +2,19 @@
 // Filtering with peco
 // go to directory
 // Build in Linux, Mac, WIndows
+// ccccc
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+func main() {
+	content, err := ioutil.ReadFile(".pj")
+	if err != nil {
+		println(err)
+	}
+	fmt.Printf("%s", content)
+	println("This is test")
+}
